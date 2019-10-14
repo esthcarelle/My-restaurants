6fbb87aaf4ef5428a333c42528b325ea552f4080
esthcarelle <esthcarelle@gmail.com>
2019-10-14 13:44:15 +0200

    add files

---
+++ app/src/androidTest/java/com/example/myrestaurantss/MainActivityInstrumentationTest.java
@@ -0,0 +1,26 @@
+package com.example.myrestaurantss;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+public class MainActivityInstrumentationTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateEditText() {
+        onView(withId(R.id.locationEditText)).perform(typeText("Portland"))
+                .check(matches(withText("Portland")));
+    }
+}
---
+++ app/src/main/java/com/example/myrestaurantss/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.myrestaurantss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+    @BindView(R.id.findRestaurantsbutton) Button mFindRestaurantsbutton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView)
+    TextView mAppNameTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mFindRestaurantsbutton.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v) {
+        if (v == mFindRestaurantsbutton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, RestaurantActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
+    }
+}
